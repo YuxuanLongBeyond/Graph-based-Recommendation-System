3c51bd81fb6749d74d09cfbbd2d8ad914bbb30d3
YuxuanLongBeyond <yuxuan.long@epfl.ch>
2020-01-02 14:48:09 +0100

    add two functions in model for laplacian

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ Created on Thu Nov 28 17:02:15 2019
 import torch
 import torch.nn as nn
 import torch.sparse as sp
-
+import numpy as np
 
 def sparse_drop(feature, drop_out):
     tem = torch.rand((feature._nnz()))
@@ -160,7 +160,43 @@ class Loss(nn.Module):
         
     def loss(self, score):
         return self.cross_entropy(score) + self.rmse(score)
+    
+def epsilon_similarity_graph(X: np.ndarray, sigma=None, epsilon=0):
+    """ X (n x d): coordinates of the n data points in R^d.
+        sigma (float): width of the kernel
+        epsilon (float): threshold
+        Return:
+        adjacency (n x n ndarray): adjacency matrix of the graph.
+    """
+    # Your code here
+    W = np.array([np.sum((X[i] - X)**2, axis = 1) for i in range(X.shape[0])])
+    typical_dist = np.mean(np.sqrt(W))
+    # print(np.mean(W))
+    c = 0.35
+    if sigma == None:
+        sigma = typical_dist * c
+    
+    mask = W >= epsilon
+    
+    adjacency = np.exp(- W / 2.0 / (sigma ** 2))
+    adjacency[mask] = 0.0
+    adjacency -= np.diag(np.diag(adjacency))
+    return adjacency
 
+def compute_laplacian(adjacency: np.ndarray, normalize: bool):
+    """ Return:
+        L (n x n ndarray): combinatorial or symmetric normalized Laplacian.
+    """
+    # Your code here
+    d = np.sum(adjacency, axis = 1)
+    d_sqrt = np.sqrt(d)  
+    D = np.diag(1 / d_sqrt)
+    if normalize:
+        L = np.eye(adjacency.shape[0]) - (adjacency.T / d_sqrt).T / d_sqrt
+        # L = np.dot(np.dot(D, np.diag(d) - adjacency), D)
+    else:
+        L = np.diag(d) - adjacency
+    return L
         
 
 
